3b1f2c9ca582f34e09e27cfb286b41d858c41c74
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-07-22 20:29:54 +0530

    AC

---
+++ MaxDepthOfBinaryTree/main.cc
@@ -0,0 +1,23 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ 
+ int dep(TreeNode* A){
+     return A==NULL ? 0 : max(dep(A->left), dep(A->right))+1;
+ }
+ 
+int Solution::maxDepth(TreeNode* A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+
+    return dep(A);
+}
+
