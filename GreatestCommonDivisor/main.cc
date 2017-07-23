b4cf363d8e0524d0918e417a5621cac7c3f0f42b
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-07-23 13:10:03 +0530

    AC

---
+++ GreatestCommonDivisor/main.cc
@@ -0,0 +1,28 @@
+Given 2 non negative integers m and n, find gcd(m, n)
+
+GCD of 2 integers m and n is defined as the greatest integer g such that g is a divisor of both m and n.
+Both m and n fit in a 32 bit signed integer.
+
+Example
+
+m : 6
+n : 9
+
+GCD(m, n) : 3 
+ NOTE : DO NOT USE LIBRARY FUNCTIONS 
+
+
+int cd(int a, int b) {
+    return b == 0 ? a : cd(b, a % b);
+}
+
+int Solution::gcd(int A, int B) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+   return cd(A,B);
+}
+
+//O(log n)
+
