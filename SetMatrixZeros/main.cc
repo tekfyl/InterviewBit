b0dc57c0e469f338b42b34abc9c15c98284a981e
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-07-23 12:35:10 +0530

    AC

---
+++ SetMatrixZeros/main.cc
@@ -0,0 +1,59 @@
+Given an m x n matrix of 0s and 1s, if an element is 0, set its entire row and column to 0.
+
+Do it in place.
+
+Example
+
+Given array A as
+
+1 0 1
+1 1 1 
+1 1 1
+On returning, the array A should be :
+
+0 0 0
+1 0 1
+1 0 1
+Note that this will be evaluated on the extra memory used. Try to minimize the space and time complexity.
+
+#define all(c) c.begin(), c.end()
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector< vi >
+#define vvl vector< vector<ll> >
+#define mk make_pair
+#define ii pair <int, int>
+#define LL pair <ll, ll>
+#define fi first
+#define se second
+#define mx max_element
+#define mn min_element
+#define rep(i,n)    for(__typeof(n) i = 0; i < n; i++)
+#define rep1(i,n)   for(__typeof(n) i = 1; i <= n; i++)
+double tick(){static clock_t oldt,newt=clock();double diff=100.0*(newt-oldt)/CLOCKS_PER_SEC;oldt=newt;return diff;}
+
+using namespace std;
+void Solution::setZeroes(vector<vector<int> > &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+     vector<ii > v;
+    rep(i,n){
+        rep(j,m){
+            if(mat[i][j] == 0) v.pb(mk(i,j));
+        }
+    }
+    rep(i, v.size()){
+        int x = v[i].fi, y = v[i].se;
+        rep(j,m){
+            mat[x][j] = 0;
+        }
+        rep(j,n){
+            mat[j][y] = 0;
+        }
+    }
+}
+
