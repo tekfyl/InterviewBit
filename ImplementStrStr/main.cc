3297f634935452605fc82ac6436f6d5ddc3713f5
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-07-23 15:56:46 +0530

    AC

---
+++ ImplementStrStr/main.cc
@@ -0,0 +1,20 @@
+Implement strStr().
+
+ strstr - locate a substring ( needle ) in a string ( haystack ). 
+Try not to use standard library string functions for this question.
+
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ NOTE: Good clarification questions:
+What should be the return value if the needle is empty?
+What if both haystack and needle are empty?
+For the purpose of this problem, assume that the return value should be -1 in both cases. 
+
+int Solution::strStr(const string &haystack, const string &needle) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    return (needle.empty() || haystack.empty()) ? -1 : (int)haystack.find(needle);
+}
+
