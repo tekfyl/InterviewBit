477ae24ee3448589c982b3c43a5210cc3a18912b
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-07-23 11:40:19 +0530

    AC

---
+++ GcdCmpl/main.cc
@@ -0,0 +1,11 @@
+int gcd(int n, int m) {
+            if (n%m ==0) return m;
+            if (n < m) swap(n, m);
+            while (m > 0) {
+                n = n%m;
+                swap(n, m);
+            }
+            return n;
+    }
+
+ Θ(logn)
