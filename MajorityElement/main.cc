b73e018a3424e40c933180c225aa772a37b57150
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-07-24 16:47:46 +0530

    AC

--- MajorityElement/main.cc
+++ MajorityElement/main.cc
@@ -1,11 +1,22 @@
+Given an array of size n, find the majority element. The majority element is the element that appears more than floor(n/2) times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example :
+
+Input : [2, 1, 2]
+Return  : 2 which occurs 2 times which is greater than 3/2. 
+
 int Solution::majorityElement(const vector<int> &A) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-    int ct=0, check = A.size()/2;
-    for(int i=0; i<A.size(); i++){
-        ct = count(A.begin(), A.end(), A[i]);
-        if(ct > check) return A[i];
+    int ct=A[0], check=-1;
+    for(int i=1; i<A.size(); i++){
+        ct == A[i] ? check-- : check++;
+        if(check == 0){ ct = A[i]; check = -1;}
     }
+    return ct;
 }
+
